52e39b0488da1c10a16ba9671ed6b72d109c0752
anthony <anthony_liang@student.uml.edu>
2024-02-23 13:20:54 -0500

    added Firebase

---
+++ src/Firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzgXbEuu_VsGfEAKf2tfCxZocdW7Rmoyc",
+  authDomain: "riverhawk-homes.firebaseapp.com",
+  projectId: "riverhawk-homes",
+  storageBucket: "riverhawk-homes.appspot.com",
+  messagingSenderId: "915964534091",
+  appId: "1:915964534091:web:b8c316ccaf111726b2dcce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// call app in other files if you want to use Firebase there
+// Firebase usage exported as "app"
+export default app;
