d3dea0c5908d1352421f11db81d3f32369cd821e
GenericSoftwareMember <kevinsree2002@gmail.com>
2024-03-27 22:32:36 -0400

    Updated styling on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Container, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import '../css/footer.css'
 
 function Footer() {
   return (
