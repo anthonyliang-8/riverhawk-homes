9422ca75a251d42fa30d537bab5c05ed94c030b4
dylans1lk <151115198+dylans1lk@users.noreply.github.com>
2024-03-31 16:22:02 -0400

    added filters to review pages

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -31,6 +31,7 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
+  Checkbox
 } from "@chakra-ui/react";
 import { Trash, ThumbsDown, ThumbsUp, Warning } from "@phosphor-icons/react";
 
@@ -49,6 +50,12 @@ function Reviews() {
   );
   const [selectedImage, setSelectedImage] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+
+  const [selectedStars, setSelectedStars] = useState([]);
+  const [filteredReviews, setFilteredReviews] = useState([]);
+  useEffect(() => {
+    setFilteredReviews(reviews);
+  }, [reviews]);
 
   /* hook to get all user IDs, implemented this just so
   we can seperate it from the admin UID */
@@ -114,6 +121,40 @@ function Reviews() {
       console.error("Error getting image URL:", error);
       return ""; // return an empty string if there's an error
     }
+  };
+
+  const updateFilter = (stars) => { // function to display only the filtered reviews
+    if (stars.length === 0) {
+      setFilteredReviews(reviews);
+    } else {
+      const filteredReviews = reviews.filter(review => stars.includes(parseInt(review.rating)));
+      setFilteredReviews(filteredReviews);
+    }
+  };
+
+  const updateCheckbox = (star) => { // function to keep track of which filters are checked/unchecked
+    const updatedStars = [...selectedStars];
+    const index = updatedStars.indexOf(star);
+    if (index === -1) {
+      updatedStars.push(star);
+    } else { 
+      updatedStars.splice(index, 1);
+    }
+
+    setSelectedStars(updatedStars);
+  }
+
+  useEffect(() => {
+    updateFilter(selectedStars);
+  }, [selectedStars]);
+
+  const drawCheckboxes = () => { // function to draw the checkboxes with the needed functionality for filtering
+    const checkboxes = [];
+    for (let i = 1; i <= 5; i++) {
+        checkboxes.push( <Checkbox key={i} onChange={() => updateCheckbox(i)}>{i} Star</Checkbox> );
+    }
+
+    return checkboxes;
   };
 
   /* !! function to handle the counts of the thumbs ups and thumbs down */
@@ -300,8 +341,24 @@ can be displayed*/
           </Button>
         </Container>
       </Box>
+      <Box display="flex" flexDirection="column" alignItems="flex-start">
+        <Box display={'flex'}
+          flexDir={'column'}
+          border={'1px solid grey'}
+          position={'absolute'}
+          left={'0'}
+          mt={'0'}
+          ml={'3vw'}
+          maxW={'md'}
+          px={'1.5vw'}
+          py={'1vw'}
+          rounded="md">
+            <Text borderBottom={"1px solid lightgrey"} mb={'0.5vw'} fontSize={'1.4em'}>Filter Reviews</Text>
+            {drawCheckboxes()}
+        </Box>
+      </Box>
       {/* Review components are mapped and displayed here */}
-      {reviews.map((review) => (
+      {filteredReviews.map((review) => (
         <Box
           border={"1px solid lightgrey"}
           borderRadius={"8px"}
