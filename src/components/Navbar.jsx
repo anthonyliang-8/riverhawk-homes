4be1e198cd2117c12e5f36fff4c9e891d7fe3246
anthony <anthony_liang@student.uml.edu>
2024-04-05 12:40:32 -0400

    added logo, modified about us page css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Box, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Flex, Spacer, Image, Text } from "@chakra-ui/react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../Firebase";
+import RiverhawkLogo from "../img/uml_riverhawk_logo.svg";
 
 const Navbar = () => {
   const [displayName, setDisplayName] = useState("");
@@ -25,20 +26,40 @@ const Navbar = () => {
   }, []);
 
   return (
-    <Box boxShadow="md" borderRadius={"12px"}>
+    <Box>
       <Flex p="4" alignItems="center">
-        <Box>
-          <Link to="/">Riverhawk Homes</Link>
+        {/* this is the logo, probably need to replace this code with an svg of the logo and the text inside instead*/}
+        <Box display={"flex"} alignItems={"center"}>
+          <Image src={RiverhawkLogo} width={"5%"} height={"auto"}></Image>
+          <Link to="/">
+            <Text fontWeight={"bold"}>Riverhawk Homes</Text>
+          </Link>
         </Box>
         <Spacer />
         {!isLoggedIn && (
           <Box marginRight={"1em"}>
-            <Link to="/login">Login</Link>
+            <Link to="/login">
+              <Text fontWeight={"600"}>Login</Text>
+            </Link>
           </Box>
         )}
         {!isLoggedIn && (
-          <Box border={'1px solid black'} borderRadius={'8px'} p={1}>
-            <Link to="/signup">Sign Up</Link>
+          <Box
+            border={"2px solid #0067B1"}
+            borderRadius={"8px"}
+            p={1}
+            fontWeight={"600"}
+            color="#0067B1"
+            transition={"all 0.3s ease-out"}
+            _hover={{
+              bg: "#0067B1",
+              color: "white",
+              transition: "all 0.3s",
+            }}
+          >
+            <Link to="/signup">
+              <Text>Sign Up</Text>
+            </Link>
           </Box>
         )}
         {isLoggedIn && displayName && (
