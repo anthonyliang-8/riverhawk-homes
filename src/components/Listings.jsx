8730ec4f25c459dcaee66f8cb34ea712478f6e7c
anthonyliang-8 <anthony_liang@student.uml.edu>
2024-04-22 16:49:11 -0400

    updated filter on home page to show checkboxes instead of slider

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,32 +1,16 @@
 import React, { useState, useEffect } from "react";
-import "../css/styles.css";
-import Dorm from "./Dorm";
-import { db, storage } from "../Firebase";
+import { Box, Text, Flex, Checkbox, RangeSlider, RangeSliderTrack, RangeSliderFilledTrack, RangeSliderThumb } from "@chakra-ui/react";
 import { collection, getDocs } from "firebase/firestore";
 import { getDownloadURL, ref } from "firebase/storage";
-import {
-  Box,
-  Text,
-  Flex,
-  Checkbox,
-  RangeSlider,
-  RangeSliderTrack,
-  RangeSliderFilledTrack,
-  RangeSliderThumb,
-} from "@chakra-ui/react";
+import Dorm from "./Dorm";
+import { db, storage } from "../Firebase";
 
 function Listings() {
   const [dorms, setDorms] = useState([]);
   const [selectedCampus, setSelectedCampus] = useState([]);
   const [filteredDorms, setFilteredDorms] = useState([]);
   const [currentPriceRange, setCurrentPriceRange] = useState([8710, 12000]);
-  const [selectedRatingRange, setSelectedRatingRange] = useState([0, 5]);
-
-  useEffect(() => {
-    setFilteredDorms(dorms);
-  }, [dorms]);
-
-  const [maxPrice, setMaxPrice] = useState(12000);
+  const [selectedRatings, setSelectedRatings] = useState(new Set());
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,7 +18,7 @@ function Listings() {
       const docs = await getDocs(dormsCollectionRef);
       const data = docs.docs.map(async (doc) => {
         const dormData = doc.data();
-        const imageURL = await getImageURL(dormData.img_path); //img_path holds path to image in Firebase storage
+        const imageURL = await getImageURL(dormData.img_path);
         return { ...dormData, id: doc.id, img_url: imageURL };
       });
       Promise.all(data).then(setDorms);
@@ -54,53 +38,33 @@ function Listings() {
     }
   };
 
-  const updateFilter = (campuses, minPrice, maxPrice, ratingRange) => {
-    let filteredDorms = dorms;
-
-    if (campuses.length > 0) {
-      filteredDorms = filteredDorms.filter((dorm) =>
-        campuses.includes(dorm.campus)
-      );
-    }
-
-    filteredDorms = filteredDorms.filter(
-      (dorm) =>
-        parseInt(dorm.price) >= parseInt(minPrice) &&
-        parseInt(dorm.price) <= parseInt(maxPrice) &&
-        (ratingRange === undefined ||
-          (dorm.rating >= ratingRange[0] && dorm.rating <= ratingRange[1]))
-    );
-    setFilteredDorms(filteredDorms);
-  };
-
-  const updateCheckboxes = (campus) => {
-    console.log(campus);
-    const updatedCampus = [...selectedCampus];
-    const index = updatedCampus.indexOf(campus);
-
-    if (index === -1) {
-      updatedCampus.push(campus);
-    } else {
-      updatedCampus.splice(index, 1);
-    }
-
-    setSelectedCampus(updatedCampus);
+  const updateFilter = () => {
+    let filtered = dorms.filter(dorm => {
+      const matchesCampus = selectedCampus.length === 0 || selectedCampus.includes(dorm.campus);
+      const matchesPrice = dorm.price >= currentPriceRange[0] && dorm.price <= currentPriceRange[1];
+      const matchesRating = selectedRatings.size === 0 || [...selectedRatings].some(rating => dorm.rating >= rating && dorm.rating < rating + 1);
+      return matchesCampus && matchesPrice && matchesRating;
+    });
+    setFilteredDorms(filtered);
   };
 
   useEffect(() => {
-    updateFilter(selectedCampus, 8710, maxPrice, selectedRatingRange);
-  }, [selectedCampus, maxPrice, selectedRatingRange]);
+    updateFilter();
+  }, [dorms, selectedCampus, currentPriceRange, selectedRatings]);
 
-  const updateMaxPrice = (range) => {
-    const [minPrice, maxPrice] = range;
-    setCurrentPriceRange(range);
-    updateFilter(selectedCampus, minPrice, maxPrice);
+  const handleRatingChange = (rating) => {
+    const newRatings = new Set(selectedRatings);
+    if (newRatings.has(rating)) {
+      newRatings.delete(rating);
+    } else {
+      newRatings.add(rating);
+    }
+    setSelectedRatings(newRatings);
+    updateFilter();
   };
 
   return (
     <Flex ml={'5em'} mr={'5em'} mb={'3em'}>
-      {/* flexbox for filter component */}
-
       <Box
         display={"flex"}
         flexDir={"column"}
@@ -109,10 +73,9 @@ function Listings() {
         mr={"3em"}
         minW={"16em"}
         minH={"10em"}
-        maxH={"20em"}
+        maxH={"25em"}
         borderRadius={'6px'}
       >
-        {/* header for top of container */}
         <Text
           fontSize="1.3em"
           fontWeight="600"
@@ -123,27 +86,17 @@ function Listings() {
         >
           Filters
         </Text>
-        <Box
-          p={'.5em'} display={'flex'}
-          flexDir={'column'}> {/* container for bottom of filter */}
+        <Box p={'.5em'} display={'flex'} flexDir={'column'}>
           <Text fontWeight="600">Location</Text>
-          <Checkbox onChange={() => updateCheckboxes("North")}>
-            North Campus
-          </Checkbox>
-          <Checkbox onChange={() => updateCheckboxes("South")}>
-            South Campus
-          </Checkbox>
-          {/* probably need to implement filtering by rating/stars here */}
-          <Text mt="1em" fontWeight="600">
-            Price
-          </Text>
-          {/* slider to handle filtering the dorms by price */}
+          <Checkbox onChange={() => updateCheckboxes("North")}>North Campus</Checkbox>
+          <Checkbox onChange={() => updateCheckboxes("South")}>South Campus</Checkbox>
+          <Text mt="1em" fontWeight="600">Price</Text>
           <RangeSlider
             defaultValue={[8710, 12000]}
             min={8710}
             max={12000}
             step={100}
-            onChange={(val) => updateMaxPrice(val)}
+            onChange={(val) => setCurrentPriceRange(val)}
           >
             <RangeSliderTrack>
               <RangeSliderFilledTrack />
@@ -151,25 +104,17 @@ function Listings() {
             <RangeSliderThumb index={0} />
             <RangeSliderThumb index={1} />
           </RangeSlider>
-          <Text>
-            ${currentPriceRange[0]} - ${currentPriceRange[1]}
-          </Text>
+          <Text>${currentPriceRange[0]} - ${currentPriceRange[1]}</Text>
           <Text mt="1em" fontWeight="600">Rating</Text>
-          <RangeSlider
-            defaultValue={[0, 5]}
-            min={0}
-            max={5}
-            step={0.1}
-            value={selectedRatingRange}
-            onChange={(val) => setSelectedRatingRange(val)}
-          >
-            <RangeSliderTrack>
-              <RangeSliderFilledTrack />
-            </RangeSliderTrack>
-            <RangeSliderThumb index={0} />
-            <RangeSliderThumb index={1} />
-          </RangeSlider>
-          <Text>Rating: {selectedRatingRange[0]} - {selectedRatingRange[1]}</Text>
+          {[1, 2, 3, 4, 5].map(rating => (
+            <Checkbox
+              key={rating}
+              isChecked={selectedRatings.has(rating)}
+              onChange={() => handleRatingChange(rating)}
+            >
+              {rating} Star
+            </Checkbox>
+          ))}
         </Box>
       </Box>
 
@@ -177,7 +122,7 @@ function Listings() {
         display={"flex"}
         flexDir={"row"}
         flexWrap={"wrap"}
-        mt={"2em"} // set to 2em since the dorm component has mt=1em
+        mt={"2em"}
         alignItems={"center"}
         justifyContent={"space-evenly"}
       >
